b085e7283a5aeec646696d7f5f6fe19a7f0a2770
Akis Kostopoulos <kostopoulosakis@gmail.com>
2022-10-25 00:27:00 +0300

    changes to db connectivity 2

--- src/main/java/gr/aueb/cf/teachersapp/dao/dbutil/DBUtil.java
+++ src/main/java/gr/aueb/cf/teachersapp/dao/dbutil/DBUtil.java
@@ -16,18 +16,18 @@ public class DBUtil {
 		try {
 			Class.forName("com.mongodb.jdbc.MongoDriver");
 		
-			String url = "jdbc:mongodb+srv://akiskost.xw2wo3c.mongodb.net/finalproj";
-			String username = "akis";
-			String password = "KvWem3SJtjG3HJmk";
-			String database = "finalproj";
- 			conn = DriverManager.getConnection(url, username, password, database);
+//			String url = "jdbc:mongodb+srv://akiskost.xw2wo3c.mongodb.net/finalproj";
+//			String username = "akis";
+//			String password = "KvWem3SJtjG3HJmk";
+//			String database = "finalproj";
+// 			conn = DriverManager.getConnection(url, username, password, database);
 
-//			java.util.Properties p = new java.util.Properties();
-//			p.setProperty("user", "akis");
-//			p.setProperty("password", "KvWem3SJtjG3HJmk");
-//			p.setProperty("database", "finalproj");
-//			System.out.println("Connecting to database...");
-//			Connection conn = DriverManager.getConnection("jdbc:mongodb+srv://akiskost.xw2wo3c.mongodb.net/?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000", p);
+			java.util.Properties p = new java.util.Properties();
+			p.setProperty("user", "akis");
+			p.setProperty("password", "KvWem3SJtjG3HJmk");
+			p.setProperty("database", "finalproj");
+			System.out.println("Connecting to database...");
+			Connection conn = DriverManager.getConnection("jdbc:mongodb+srv://akiskost.xw2wo3c.mongodb.net/?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000", p);
 // 			Connection conn = DriverManager.getConnection("jdbc:mongodb//akis:X3RLbnYWCUXE8qyA@akiskost.xw2wo3c.mongodb.net/?retryWrites=true&w=majority");
 
 
